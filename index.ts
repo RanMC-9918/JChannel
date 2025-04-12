dc373a67ee94c17dc89c6c9e2506000f54231d7f
RanMC-9918 <rohanjain0725@gmail.com>
2025-04-12 13:39:18 -0500

    fg

--- index.ts
+++ index.ts
@@ -21,9 +21,10 @@ interface Game {
   gameType: string;
   gameTime: number;
   gameMaxPlayers: number;
+  gameID: number;
 }
 
-let games: Game[];
+let games: Game[] = [];
 
 // Set EJS as the templating engine
 app.set("view engine", "ejs");
@@ -56,15 +57,58 @@ app.get("/create/game", (req, res) => {
   res.render(path.join("create", "game"));
 });
 
+app.get("/dashboard", (req, res) => {
+  res.render(path.join("dashboard", "index"));
+});
+
 app.post("/api/create/game", (req, res) => {
   let settings = req.body;
 
+  if(!settings) {
+    console.warn("No settings provided");
+    return;
+  }
+
+  let id = Math.floor(Math.random() * 1000000); // Generate a random game ID
+
+  if(!settings.gameType) {
+    console.warn("No gametype");
+    return;
+  }
+  if(!settings.gameTime) {
+    console.warn("No gametime");
+    return;
+  }
+  if(!settings.gameMaxPlayers) {
+    console.warn("No max players");
+    return;
+  }
+
   games.push({
-    teams: settings.teams,
+    teams: [],
     gameType: settings.gameType,
     gameTime: settings.gameTime,
     gameMaxPlayers: settings.gameMaxPlayers,
+    gameID: id,
   });
+
+  res.send(JSON.stringify({ gameID: id }));
+});
+
+app.get("/api/game", (req, res) => {
+  let gameID = req.query.gameID;
+  if (!gameID) {
+    console.warn("No game ID provided");
+    return;
+  }
+
+  let game = games.find((game) => game.gameID == gameID);
+  if (!game) {
+    console.warn("Game not found");
+    return;
+  }
+
+  res.send(JSON.stringify(game));
 });
 
 // Start the server
--- views/create/game/create.js
+++ views/create/game/create.js
@@ -1,1 +1,32 @@
-let 
+let submit = document.getElementById("submit");
+
+let players = document.getElementById("players");
+let time = document.getElementById("time")
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    createGame();
+});
+
+function createGame(){
+    if (players.value.length > 0 && time.value.length > 0) {
+      fetch("/api/create/game", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          gameMaxPlayers: players.value,
+          gameTime: time.value,
+          gameType: window.location.href.substring(
+            window.location.href.lastIndexOf("/?type=") + 7
+          ),
+        }),
+      })
+        .then((data) => data.json())
+        .then((res) => {
+          window.location.href = "/dashboard/" + res.gameID;
+        });
+    }
+}
+
